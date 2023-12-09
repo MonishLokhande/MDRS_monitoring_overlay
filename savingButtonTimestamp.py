e29d89ec9a09973fe80397e1133f98075bb6833b
Nathan Bitner <nsbitner@purdue.edu>
2023-12-09 00:44:10 -0500

    Added initial html file and button status logger

---
+++ savingButtonTimestamp.py
@@ -0,0 +1,28 @@
+# Doesn't run 
+# TODO set up for pi pico
+
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(13, GPIO.IN)
+GPIO.setup(6, GPIO.OUT)
+
+GPIO.output(6, GPIO.LOW)
+
+timestamp = datetime.now().strftime('%b_%d_%Y-%H-%M')
+pressLog = open("{}_button_log.txt".format(timestamp), 'w')
+
+try:
+    while(1):
+        if GPIO.input(13):
+            GPIO.output(6, GPIO.HIGH)
+            pressLog.write(datetime.now().strftime('%b_%d-%H-%M-%S'+": ON\n"))
+        else:
+            GPIO.output(6, GPIO.LOW)
+            pressLog.write(datetime.now().strftime('%b_%d-%H-%M-%S'+": OFF\n"))
+        
+        GPIO.output(26, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(26, GPIO.LOW)
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    pressLog.close()
